1bcc07076a9beb3b2770bd051ad2fa766470eef3
YernarT <toktaryernar@gmail.com>
2023-04-10 15:34:09 +0600

    init: :rainbow: Button 组件

---
+++ frontend/src/components/Button/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<button class="button">
+		<slot></slot>
+	</button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import '@/assets/style/variable.scss';
+
+.button {
+	color: #fff;
+	text-align: center;
+
+	padding: 14px 20px;
+	outline: none;
+	border: none;
+	border-radius: var(--border-radius);
+	background-color: var(--c-primary);
+
+	cursor: pointer;
+	transition: background-color var(--transition);
+
+	&:active {
+		background-color: lighten($color: $primaryColor, $amount: 10%);
+	}
+}
+</style>
