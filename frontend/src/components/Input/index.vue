2d59b66f8981c5537b4ac2fb52f80d0a2b2eb228
YernarT <toktaryernar@gmail.com>
2023-04-10 15:34:17 +0600

    init: :rainbow: Input 组件

---
+++ frontend/src/components/Input/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<input class="input" :type="type" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+defineProps({
+	type: {
+		type: String,
+		default: 'text',
+	},
+
+	value: {
+		type: String,
+		required: true,
+	},
+});
+
+const emit = defineEmits(['change']);
+
+const handleChange = (payload: Event) => {
+	emit('change', (payload.target as HTMLInputElement).value);
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/variable.scss';
+
+.input {
+	padding: 12px 16px;
+	color: inherit;
+
+	outline: none;
+	border: 1px solid #cad6e4;
+	border-radius: var(--border-radius);
+
+	transition: border-color var(--transition);
+
+	&:focus {
+		border-color: var(--c-primary);
+	}
+}
+</style>
