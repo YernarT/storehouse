7c473cb935b8f0c8d772c81333214354ec650784
YernarT <toktaryernar@gmail.com>
2023-04-18 01:17:08 +0600

    update: :hammer: add icon props

--- frontend/src/components/Button/index.vue
+++ frontend/src/components/Button/index.vue
@@ -1,13 +1,29 @@
 <template>
-	<button class="button">
+	<button
+		class="button"
+		:class="{ 'button--disabled': isDisable, 'button--icon-btn': isIconBtn }"
+	>
 		<slot></slot>
 	</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+	isDisable: {
+		type: Boolean,
+		default: false,
+	},
+
+	isIconBtn: {
+		type: Boolean,
+		default: false,
+	},
+});
+</script>
 
 <style scoped lang="scss">
 @import '@/assets/style/variable.scss';
+@import '@/assets/style/mixins.scss';
 
 .button {
 	color: #fff;
@@ -24,6 +40,19 @@
 
 	&:active {
 		background-color: lighten($color: $primaryColor, $amount: 10%);
+	}
+
+	&--icon-btn {
+		@include flexCenter();
+
+		:deep(svg) {
+			width: 32px;
+			height: 32px;
+		}
+	}
+
+	&--disabled {
+		background: darken($color: $primaryColor, $amount: 0.1);
 	}
 }
 </style>
