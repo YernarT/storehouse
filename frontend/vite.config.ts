8f1eaa466105c816b18e9277369a1e2f9d0a2031
YernarT <toktaryernar@gmail.com>
2023-04-26 17:33:05 +0600

    refactor: :recycle: vue3 -> react17

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 import path from 'path';
 import autoprefixer from 'autoprefixer';
 import sass from 'sass';
-
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,15 +13,7 @@ export default defineConfig({
 		},
 	},
 
-	plugins: [
-		vue(),
-		AutoImport({
-			resolvers: [ElementPlusResolver()],
-		}),
-		Components({
-			resolvers: [ElementPlusResolver()],
-		}),
-	],
+	plugins: [react()],
 
 	css: {
 		postcss: {
