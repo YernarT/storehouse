ec70458477e2df8bfa8b90556a69de56cc47b583
YernarT <toktaryernar@gmail.com>
2023-04-26 17:35:12 +0600

    feat: :sparkles: user atom

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,1 +1,7 @@
-export { useUserStore } from './user';
+// User Atoms, Selectors
+export type { userStateProperties } from '@/store/user-atom';
+export {
+	A_User,
+	defaultUserState,
+	S_UserIsAuthenticated,
+} from '@/store/user-atom';
---
+++ frontend/src/store/user-atom.ts
@@ -0,0 +1,33 @@
+import type { I_User } from '@/def_types/user';
+
+import { atom, selector } from 'recoil';
+import { localStorage } from '@/utils';
+
+export interface userStateProperties extends I_User {}
+
+export const defaultUserState: userStateProperties = {
+	id: 0,
+	token: '',
+	phone: '',
+	fullname: '',
+	isStaff: false,
+	createTime: '',
+};
+
+const state = localStorage.get('user', defaultUserState);
+
+export const A_User = atom({
+	key: 'A_User',
+	// default value, aka initial value
+	default: state,
+});
+
+export const S_UserIsAuthenticated = selector({
+	key: 'S_UserIsAuthenticated',
+	get({ get }) {
+		const user = get(A_User);
+		const hasToken = user.token !== defaultUserState.token;
+
+		return hasToken;
+	},
+});
