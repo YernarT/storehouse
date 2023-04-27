a2a3c6ce2c9687b3a521deb48bf9c36799b9ce2d
YernarT <toktaryernar@gmail.com>
2023-04-27 13:27:17 +0600

    refactor: :recycle: vue3 -> react17

---
+++ frontend/src/components/common/SafeArea/index.tsx
@@ -0,0 +1,73 @@
+// Router
+import { useHistory, useLocation } from 'react-router-dom';
+// Recoil
+import { useRecoilValue } from 'recoil';
+import { A_User } from '@/store';
+
+// Hooks
+import { useCreation, useMemoizedFn } from 'ahooks';
+
+// Antd component
+import { Button } from 'antd';
+// Custom component
+import { RouteGuard } from '@/components/common';
+// Icons
+import { BiUser } from 'react-icons/bi';
+import { MdOutlineQrCodeScanner } from 'react-icons/md';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+
+// Scoped style
+import classes from './style.module.scss';
+
+// Routes
+import routes from '@/routes';
+
+export default function SafeArea() {
+	const user = useRecoilValue(A_User);
+	const history = useHistory();
+	const location = useLocation();
+
+	const showFloatBtn = useCreation(() => {
+		const disabledPages = ['/', '/login'];
+		return !disabledPages.includes(location.pathname);
+	}, [location.pathname]);
+
+	const floatBtnIcon = useCreation(() => {
+		if (user.isStaff) {
+			return <MdOutlineQrCodeScanner />;
+		}
+
+		if (location.pathname === '/profile') {
+			return <IoMdArrowRoundBack />;
+		}
+
+		return <BiUser />;
+	}, [user.isStaff, location.pathname]);
+
+	const handleFloatBtn = useMemoizedFn(() => {
+		if (user.isStaff) {
+			return;
+		}
+
+		if (location.pathname === '/profile') {
+			history.goBack();
+			return;
+		}
+
+		history.push('/profile');
+	});
+
+	return (
+		<div className={classes.safeArea}>
+			<RouteGuard routes={routes} />
+			{showFloatBtn && (
+				<Button
+					className="float-btn"
+					type="primary"
+					icon={floatBtnIcon}
+					onClick={handleFloatBtn}
+				/>
+			)}
+		</div>
+	);
+}
