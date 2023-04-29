99cc027a4136395601e687137f8a3d5c3e2774e0
YernarT <toktaryernar@gmail.com>
2023-04-29 23:40:00 +0600

    feat: :sparkles: all about profile

---
+++ frontend/src/components/profile/index.tsx
@@ -0,0 +1,3 @@
+export { default as ProfileForm } from './ProfileForm';
+
+export { default as MyTicket } from './MyTicket';
--- frontend/src/pages/ProfilePage/index.tsx
+++ frontend/src/pages/ProfilePage/index.tsx
@@ -1,6 +1,31 @@
+// Types
+import type { TabsProps } from 'antd';
+
+// Antd component
+import { Tabs } from 'antd';
+// Custom component
+import { ProfileForm, MyTicket } from '@/components/profile';
+
 // Scoped style
 import classes from './style.module.scss';
 
+const items: TabsProps['items'] = [
+	{
+		key: 'profileForm',
+		label: 'Профиль',
+		children: <ProfileForm />,
+	},
+	{
+		key: 'MyTicket',
+		label: 'Билеттерім',
+		children: <MyTicket />,
+	},
+];
+
 export default function ProfilePage() {
-	return <main className={classes.profilePage}>ProfilePage</main>;
+	return (
+		<main className={classes.profilePage}>
+			<Tabs defaultActiveKey="profileForm" items={items} />
+		</main>
+	);
 }
