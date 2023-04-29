8652999e9b9232da265e65dd19d868b80c9702b5
YernarT <toktaryernar@gmail.com>
2023-04-29 23:39:17 +0600

    init: :rainbow: my ticket component

---
+++ frontend/src/components/profile/MyTicket/index.tsx
@@ -0,0 +1,38 @@
+// Types
+import { I_Ticket } from '@/def_types/ticket';
+
+// React
+import { useState } from 'react';
+
+// Hooks
+import { useRequest } from 'ahooks';
+// API
+import { API_GetAllTicket } from '@/service/ticket-api';
+
+// Antd component
+import { Empty } from 'antd';
+// Custom component
+import { TicketCard } from '@/components/ticket';
+
+// Scoped style
+import classes from './style.module.scss';
+
+export default function MyTicket() {
+	const [ticketList, setTicketList] = useState<I_Ticket[]>([]);
+
+	// fetch ticket list
+	useRequest(API_GetAllTicket, {
+		onSuccess(data) {
+			setTicketList(data);
+		},
+	});
+
+	return (
+		<div className={classes.myTicket}>
+			{ticketList.map(ticket => (
+				<TicketCard key={ticket.id} ticket={ticket} />
+			))}
+			{ticketList.length === 0 && <Empty description="Билет жоқ" />}
+		</div>
+	);
+}
