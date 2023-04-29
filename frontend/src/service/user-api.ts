f0a9cf8716e081332fc223ae303337f7abf00084
YernarT <toktaryernar@gmail.com>
2023-04-29 23:07:19 +0600

    update: :hammer: update profile

--- frontend/src/service/user-api.ts
+++ frontend/src/service/user-api.ts
@@ -10,3 +10,11 @@ export interface API_LoginData {
 
 export const API_Login = (data: API_LoginData) =>
 	fetch.post<I_User>('/login', data);
+
+export interface API_UpdateProfileData {
+	fullname: string;
+	phone: string;
+}
+
+export const API_UpdateProfile = (id: number, data: API_UpdateProfileData) =>
+	fetch.patch<I_User>(`/user/${id}/`, data);
