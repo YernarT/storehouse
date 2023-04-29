76127a3fa67d7cffbe90eaa8160772bedf9e6007
YernarT <toktaryernar@gmail.com>
2023-04-29 23:06:43 +0600

    update: :hammer: add more methods

--- frontend/src/service/fetch.ts
+++ frontend/src/service/fetch.ts
@@ -13,6 +13,41 @@ const BASE_URL = `${HOST}:${PORT}/api`;
 import { localStorage, objectToUrlParams, convertKeysCase } from '@/utils';
 import { defaultUserState } from '@/store/user-atom';
 
+function requestWithData(method: 'POST' | 'PATCH' | 'PUT' | 'DELETE') {
+	return async <T>(url: string, data?: object): Promise<T> => {
+		if (data) {
+			data = convertKeysCase(data, 'snake');
+		}
+
+		// before fetch
+		const { token } = localStorage.get('user', defaultUserState);
+		url = `${BASE_URL}${url}`;
+
+		const response = await fetch(url, {
+			method,
+			body: JSON.stringify(data),
+			headers: {
+				Authorization: token,
+				'Content-Type': 'application/json',
+			},
+		}).catch(() => {
+			return { isSuccess: false, message: 'Сервер уақытша жабық' };
+		});
+
+		if (response instanceof Response) {
+			data = await response.json();
+
+			if (response.status >= 200 && response.status < 300) {
+				return convertKeysCase(data!, 'camel') as any as T;
+			}
+
+			return await Promise.reject(data);
+		}
+
+		return await Promise.reject(response);
+	};
+}
+
 export default {
 	async get<T>(url: string, params?: object): Promise<T> {
 		if (params) {
@@ -46,36 +81,8 @@ export default {
 		return await Promise.reject(response);
 	},
 
-	async post<T>(url: string, data?: object): Promise<T> {
-		if (data) {
-			data = convertKeysCase(data, 'snake');
-		}
-
-		// before fetch
-		const { token } = localStorage.get('user', defaultUserState);
-		url = `${BASE_URL}${url}`;
-
-		const response = await fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				Authorization: token,
-				'Content-Type': 'application/json',
-			},
-		}).catch(() => {
-			return { isSuccess: false, message: 'Сервер уақытша жабық' };
-		});
-
-		if (response instanceof Response) {
-			data = await response.json();
-
-			if (response.status >= 200 && response.status < 300) {
-				return convertKeysCase(data!, 'camel') as any as T;
-			}
-
-			return await Promise.reject(data);
-		}
-
-		return await Promise.reject(response);
-	},
+	post: requestWithData('POST'),
+	patch: requestWithData('PATCH'),
+	put: requestWithData('PUT'),
+	delete: requestWithData('DELETE'),
 };
