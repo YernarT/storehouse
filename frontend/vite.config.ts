8b58e7fe5855e95fae7db174c40c10d590148cff
YernarT <toktaryernar@gmail.com>
2023-06-13 00:00:32 +0600

    chore: :wrench: default host, port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,38 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import path from 'path';
-import autoprefixer from 'autoprefixer';
-import sass from 'sass';
+import path from "path";
+import autoprefixer from "autoprefixer";
+import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(path.resolve(), './src'),
-		},
-	},
+  server: {
+    host: "0.0.0.0",
+    port: 3200,
+  },
 
-	plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(path.resolve(), "./src"),
+    },
+  },
 
-	css: {
-		postcss: {
-			plugins: [
-				autoprefixer({
-					overrideBrowserslist: 'last 2 versions',
-				}),
-			],
-		},
+  plugins: [react()],
 
-		preprocessorOptions: {
-			sass: {
-				implementation: sass,
-			},
-		},
-	},
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: "last 2 versions",
+        }),
+      ],
+    },
+
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
 });
