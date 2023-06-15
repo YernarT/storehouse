fc745cd753d6fc2008b12edbb396f40378e5929f
YernarT <toktaryernar@gmail.com>
2023-06-15 11:15:24 +0600

    update: :hammer: change icon

--- frontend/src/components/common/SafeArea/index.tsx
+++ frontend/src/components/common/SafeArea/index.tsx
@@ -1,75 +1,51 @@
 // Router
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from "react-router-dom";
 // Recoil
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { A_User, A_Page } from '@/store';
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { A_User, A_Page } from "@/store";
 
 // Hooks
-import { useCreation, useMemoizedFn } from 'ahooks';
+import { useCreation, useMemoizedFn } from "ahooks";
 
 // Antd component
-import { Button } from 'antd';
+import { Button } from "antd";
 // Custom component
-import { RouteGuard } from '@/components/common';
+import { RouteGuard } from "@/components/common";
 // Icons
-import { BiUser } from 'react-icons/bi';
-import { MdOutlineQrCodeScanner } from 'react-icons/md';
-import { IoMdArrowRoundBack } from 'react-icons/io';
+import { AiOutlinePlus } from "react-icons/ai";
 
 // Scoped style
-import classes from './style.module.scss';
+import classes from "./style.module.scss";
 
 // Routes
-import routes from '@/routes';
+import routes from "@/routes";
 
 export default function SafeArea() {
-	const user = useRecoilValue(A_User);
-	const setPage = useSetRecoilState(A_Page);
-	const history = useHistory();
-	const location = useLocation();
+  const user = useRecoilValue(A_User);
+  const setPage = useSetRecoilState(A_Page);
+  const history = useHistory();
+  const location = useLocation();
 
-	const showFloatBtn = useCreation(() => {
-		const disabledPages = ['/', '/login'];
-		return !disabledPages.includes(location.pathname);
-	}, [location.pathname]);
+  const showFloatBtn = useCreation(() => {
+    const disabledPages = ["/"];
+    return !disabledPages.includes(location.pathname);
+  }, [location.pathname]);
 
-	const floatBtnIcon = useCreation(() => {
-		if (user.isStaff) {
-			return <MdOutlineQrCodeScanner />;
-		}
+  const handleFloatBtn = useMemoizedFn(() => {
+    console.log("handle float btn");
+  });
 
-		if (location.pathname === '/profile') {
-			return <IoMdArrowRoundBack />;
-		}
-
-		return <BiUser />;
-	}, [user.isStaff, location.pathname]);
-
-	const handleFloatBtn = useMemoizedFn(() => {
-		if (user.isStaff) {
-			setPage(prevPage => ({ scannerIsVisible: !prevPage.scannerIsVisible }));
-			return;
-		}
-
-		if (location.pathname === '/profile') {
-			history.goBack();
-			return;
-		}
-
-		history.push('/profile');
-	});
-
-	return (
-		<div className={classes.safeArea}>
-			<RouteGuard routes={routes} />
-			{showFloatBtn && (
-				<Button
-					className="float-btn"
-					type="primary"
-					icon={floatBtnIcon}
-					onClick={handleFloatBtn}
-				/>
-			)}
-		</div>
-	);
+  return (
+    <div className={classes.safeArea}>
+      <RouteGuard routes={routes} />
+      {showFloatBtn && (
+        <Button
+          className="float-btn"
+          type="primary"
+          icon={<AiOutlinePlus />}
+          onClick={handleFloatBtn}
+        />
+      )}
+    </div>
+  );
 }
