51cce9579cc944fddaf3dd856539fd68c6d5a468
YernarT <toktaryernar@gmail.com>
2023-06-15 11:10:04 +0600

    update: :hammer: allowed origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,8 +23,8 @@ APPEND_SLASH = False
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    'http://localhost:3200',
+    'http://127.0.0.1:3200',
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
