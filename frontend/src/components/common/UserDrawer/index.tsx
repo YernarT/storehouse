b21aacf341e6f24860a9f3f0547ecaf71893de92
YernarT <toktaryernar@gmail.com>
2023-06-16 09:42:30 +0600

    feat: :sparkles: userdrawer

---
+++ frontend/src/components/common/UserDrawer/index.tsx
@@ -0,0 +1,72 @@
+// Recoil
+import { useRecoilValue, useRecoilState } from "recoil";
+import { A_User, A_Page } from "@/store";
+
+// Hooks
+import { useSetState, useEventListener, useMemoizedFn } from "ahooks";
+
+// Antd component
+import {} from "antd";
+// Icons
+import { AiOutlineUser } from "react-icons/ai";
+
+// Scoped style
+import classes from "./style.module.scss";
+
+export default function UserDrawer() {
+  const user = useRecoilValue(A_User);
+  const [page, setPage] = useRecoilState(A_Page);
+
+  const [state, setState] = useSetState({
+    toucheX: null as number | null,
+  });
+
+  useEventListener("touchstart", (e) => {
+    setState({ toucheX: e.touches[0].clientX });
+  });
+  const touchhandler = useMemoizedFn((e: TouchEvent) => {
+    const currentTouchX = e.touches[0].clientX;
+    const distance = 200;
+
+    if (state.toucheX !== null && currentTouchX - state.toucheX > distance) {
+      setPage((prevState) => ({ ...prevState, userDrawerIsOpen: true }));
+    }
+
+    if (state.toucheX !== null && currentTouchX - state.toucheX < -distance) {
+      setPage((prevState) => ({ ...prevState, userDrawerIsOpen: false }));
+    }
+  });
+  useEventListener("touchmove", touchhandler);
+  useEventListener("touchend", () => {
+    setState({ toucheX: null });
+  });
+
+  return (
+    <div
+      className={`${classes.userDrawer} ${
+        page.userDrawerIsOpen && classes.userDrawerOpened
+      }`}
+    >
+      <div
+        className="mask"
+        onClick={() =>
+          setPage((prevState) => ({ ...prevState, userDrawerIsOpen: false }))
+        }
+      />
+      <div className="body">
+        <div className="head">
+          <AiOutlineUser className="avatar" />
+          <span className="phone">{user.phone}</span>
+        </div>
+
+        {/* @TODO:
+            1. Profile
+            2. Settings
+            3. About
+            4. 意见箱
+            5. Logout
+        */}
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { default as RouteGuard } from './RouteGuard';
 
 export { default as SafeArea } from './SafeArea';
+
+export { default as UserDrawer } from './UserDrawer';
