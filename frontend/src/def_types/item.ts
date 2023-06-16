2dc4a072fe03c50b114330bce29ae1cae4ee4511
YernarT <toktaryernar@gmail.com>
2023-06-16 23:35:06 +0600

    init: :rainbow: item module types

---
+++ frontend/src/def_types/item.ts
@@ -0,0 +1,22 @@
+import type { I_User } from "./user";
+
+export interface I_Item {
+  id: number;
+  name: string;
+  description: string;
+  quantity: number;
+  purchasePrice: Number;
+  sellingPrice: Number;
+  expirationDate: Date;
+  productionDate: Date;
+  owner: I_User;
+  supplier: string;
+  shelfPosition: string;
+}
+
+export interface I_Tag {
+  id: number;
+  name: string;
+  color: string;
+  backgroundColor: string;
+}
--- frontend/src/def_types/ticket.ts
+++
@@ -1,8 +0,0 @@
-export interface I_Ticket {
-	id: number;
-	name: string;
-	isMine: boolean;
-	purchaseTime: Date;
-	expirationDate: Date;
-	totalSold?: number;
-}
