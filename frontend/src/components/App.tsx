74cc9c4c60e45ec063ee7b57f92fe5b92d44cf05
YernarT <toktaryernar@gmail.com>
2023-06-20 23:50:12 +0600

    update: :hammer: dayjs locale

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,32 +1,37 @@
 // React router
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 // Antd Component
-import { ConfigProvider as AntdConfigProvider, App as AntdApp } from 'antd';
+import { ConfigProvider as AntdConfigProvider, App as AntdApp } from "antd";
 // Component
-import { SafeArea } from '@/components/common';
+import { SafeArea } from "@/components/common";
 
 // Locale
-import kkKZ from 'antd/lib/locale/kk_KZ';
+import kkKZ from "antd/lib/locale/kk_KZ";
+import dayJs from "dayjs";
+import "dayjs/locale/kk";
+
+dayJs.locale("kk");
 
 export default function App() {
-	return (
-		// 路由管理
-		<BrowserRouter>
-			{/* Antd UI */}
+  return (
+    // 路由管理
+    <BrowserRouter>
+      {/* Antd UI */}
 
-			<AntdConfigProvider
-				theme={{
-					token: {
-						colorPrimary: '#2176ff',
-						borderRadius: 4,
-					},
-				}}
-				locale={kkKZ}>
-				<AntdApp>
-					<SafeArea />
-				</AntdApp>
-			</AntdConfigProvider>
-		</BrowserRouter>
-	);
+      <AntdConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#2176ff",
+            borderRadius: 4,
+          },
+        }}
+        locale={kkKZ}
+      >
+        <AntdApp>
+          <SafeArea />
+        </AntdApp>
+      </AntdConfigProvider>
+    </BrowserRouter>
+  );
 }
